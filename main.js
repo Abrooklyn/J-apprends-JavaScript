a298cb7262dd31def211c6d3be02e4adacb4fc44
Unknown <angeliquevannespenne@hotmail.fr>
2018-11-28 11:32:09 +0100

    On Continue Le Cours
    Udemy : Antho Welc

--- main.js
+++ main.js
@@ -1,6 +1,110 @@
-// Code JavaScript ici
+//VARIABLES :
 
-// Afficher un message dans la console du navigateur (F12 au clavier)
-console.log("Coucou");
+/*
 
-alert("Hello");
+var num = 10;
+
+var chiffre = 0;
+
+console.log(chiffre + num);
+
+chiffre = 10;
+
+console.log(chiffre + num)*/
+
+//NOMMER LES VARIABLES : 
+
+/*
+
+var motDePasse = 1234;
+
+Toujours mettre 
+
+"lePremierMotEnMinuscule"
+
+et le reste avec des majuscules
+
+*/
+
+//LES CALCULS
+
+/*
+
+console.log(11 + 2);
+
+console.log(11 * 2);
+
+console.log(11 - 2);
+
+console.log(11 / 2);
+
+console.log(11.78952 + 2);
+
+console.log(11,78952 * 2, 23);
+
+*/
+
+//LES ADDITIONS +=
+
+/*
+
+var num = 5;
+var += 5;
+
+num /= 5;
+
+num *= 5;
+
+num *= 5 + 5;
+
+num *= 5 + 5 / 2 + (5 + 4);
+
+var num = 5;
+num *= 5 + 5 / 2 + (5 + 4);
+console.log(5 + 5 / 2 + (5 + 4));
+console.log(num)
+console.log(5 * 42.5);
+
+*/
+
+//STRING
+
+/*
+
+var texte = "Coucou";
+console.log("Hello");
+
+var autreTexte = 'Bonjour';
+console.log(autreTexte);
+
+var autretexte = 'Bonjour je m\'appelle';
+var texte = "Coucou \"Antho\"";
+
+var texte = "Coucou \nAntho";
+console.log(texte);
+
+var texte = "Coucou \\Antho";
+
+*/
+
+//COMBINER DU TEXTE
+
+/*
+
+var bonjour = "Bonjour";
+var nom = "Antho";
+var resultat = bonjour + " " + nom;
+
+console.log(bonjour + " " + nom);
+console.log(resultat);
+
+bonjour += " " + nom;
+console.log(bonjour);
+
+*/
+
+var nom = "Angie";
+var age = 24;
+
+var resultat = "Bonjour " + nom + " tu as "+ age + " ans !";
+console.log(resultat);
