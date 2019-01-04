a999e1933db4c76993b983a7eaa7c7a7001473d2
Unknown <angeliquevannespenne@hotmail.fr>
2019-01-04 12:26:09 +0100

    Focus Blur Load

---
+++ focusBlur.js
@@ -0,0 +1,11 @@
+//FOCUS ET BLUR
+
+const input = document.querySelector("input");
+
+input.addEventListener("focus", function() {
+    input.style.background = "#C0FFEE";
+});
+
+input.addEventListener("blur", function() {
+    input.style.background = "white";
+});
