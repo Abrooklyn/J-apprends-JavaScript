0f46e2f1a831998bca86e6ecf5428e1eff71790f
Unknown <angeliquevannespenne@hotmail.fr>
2019-01-07 10:25:46 +0100

    Déclenher un script au bout de X secondes

--- main3.js
+++ main3.js
@@ -1,5 +1,5 @@
 //Attendre qu'un element soit chargé
-
+/*
 const images = document.querySelectorAll("img"); 
 
 const imagesArray = Array.from(images);
@@ -8,3 +8,14 @@ imagesArray
         .map((image, index) => image.addEventListener("load", function() {
                 console.log(`Image n°${index + 1}: OK`);
 }))
+*/
+
+// PRATIQUE Déclenher un script au bout de X secondes
+
+const titre = document.querySelector("h1");
+
+const texte = setTimeout(function() {
+        titre.textContent = "Le JavaScript s'apprend avec patience.";
+        titre.style.opacity = 1;
+        document.body.style.background = "#FB1";
+}, 2000)
